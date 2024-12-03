adf03399ab9f5ceaf0ef1b7d9c3f57141fd7864c
Dylan Hojnoski <dylanhoj@gmail.com>
2024-12-02 21:23:24 -0500

    Change base url

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "dylanhojnoski.github.io/obsidian-graphs-wiki",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
