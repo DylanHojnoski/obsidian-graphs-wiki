830ecd27dcfed686b2fa6e5a98a40835b43ed6f4
Dylan Hojnoski <dylanhoj@gmail.com>
2024-12-04 13:20:53 -0500

    Added icon

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Obsidian Graphs",
-    pageTitleSuffix: "",
+    pageTitleSuffix: " - Graphs Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
